baacb29b393a7808611c4d3ae4c50998dd0f6654
crv342 <chiragrvv@gmail.com>
2021-03-06 05:52:57 +0530

    qr code added

--- sevi_web/clientapp/views.py
+++ sevi_web/clientapp/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from collections import OrderedDict
 import json
 import pyrebase
+import qrcode,secrets,string,os
 
 firebaseConfig = {
     'apiKey': "AIzaSyDHECxnjEG1AmiN_pbbunQQPEyUW_Ffl34",
@@ -17,7 +18,7 @@ firebaseConfig = {
 }
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
-storage = firebase.storage()
+store = firebase.storage()
 firstname = ""
 # Create your views here.
 def index(request):
@@ -28,12 +29,17 @@ def index(request):
     else:
         firstname = request.session["username"]
         hotalid = request.session["hotelid"]
-        tables = db.child("Hotel").child(hotalid).child("tables").get()
-        for values in tables.each():
-            li.append((values.key(),values.val()))
-        li.sort(key = lambda x: int(x[0][5:]))
-        li = OrderedDict(li)
-        return render(request, "index.html", {"tables": li,  "personname" : firstname})
+        qrcode = db.child("Hotel").child(hotalid).child("QRimage").get().val()
+        print(qrcode)
+        try:
+            tables = db.child("Hotel").child(hotalid).child("tables").get()
+            for values in tables.each():
+                li.append((values.key(),values.val()))
+            li.sort(key = lambda x: int(x[0][5:]))
+            li = OrderedDict(li)
+        except:
+            pass
+        return render(request, "index.html", {"tables": li,  "personname" : firstname,"qrcode": qrcode})
 
 
 def tabledetails(request):
@@ -92,9 +98,20 @@ def signup(request):
         hd = db.child("Hotel").get().val()
         hid = "hotel"+str(len(hd)+1)
         if not check:
+            res = ''.join(secrets.choice(string.ascii_letters+string.digits) for i in range(10))
+            qrstring = hid+res
+            #Creating an instance of qrcode
+            qr = qrcode.QRCode(version=1,box_size=10,border=5)
+            qr.add_data(qrstring)
+            qr.make(fit=True)
+            qrimg = qr.make_image(fill='black', back_color='white')
+            qrimg.save(hid+"qr.jpg")
+            store.child("images").child(hid).child(hid+"qr.jpg").put(hid+"qr.jpg")
+            os.remove(hid+"qr.jpg")
+            qr_url = store.child("images").child(hid).child(hid+"qr.jpg").get_url(None)
             val={"email":email,"firstname":fname,"hotelid":hid,"hotelname":None,"lastname":lname,"password":passw,"phoneno":num}
             db.child("Web").child("hotelusers").child(num).set(val)
-            db.child("Hotel").child(hid).set({"items":"","orders":"","totaltables":0})
+            db.child("Hotel").child(hid).set({"items":"","orders":"","totaltables":0,"QRstring":qrstring,"QRimage":qr_url})
             return HttpResponseRedirect(reverse("index"))
         else:
             status="User already exits !!! Please log in "
@@ -119,7 +136,7 @@ def myhotel(request):
             ttables = request.POST["tables"]
             city = request.POST["city"]
             address = request.POST["address"]
-            img = request.POST["image"]
+            img = request.FILES["image"]
             firstname = request.POST["firstname"]
             lastname = request.POST["lastname"]
             phone = request.POST["phone"]
@@ -136,7 +153,9 @@ def myhotel(request):
                     up_table["table"+str(i)]={"status":"free"}
                 db.child("Hotel").child(hotelid).child("tables").update(up_table)
             if img:
-                db.child("Hotel").child(hotelid).update({"hotelimage": img})
+                store.child("images").child(hotelid).child(hotelid+".jpg").put(img)
+                img_url = store.child("images").child(hotelid).child("items/item1.jpg").get_url(None)
+                db.child("Hotel").child(hotelid).update({"hotelimage": img_url})
             if npassword:
                 cpass = db.child("Web").child("hotelusers").child(userid).child("password").get().val()
                 if cpass==cpassword:
@@ -171,19 +190,24 @@ def additem(request):
     b = {"u":"disabled","a":""}
     err = {}
     if request.method=="POST":
-        try:
+        #try:
             itemname = request.POST["itemname"]
             catagory = request.POST["catagory"]
             price = request.POST["price"]
             desc = request.POST["description"]
-            img = request.POST["image"]
+            if 'image' in request.FILES:
+                img = request.FILES["image"]
+            else:
+                img = ""
             item_status = request.POST.get('itemstatus',"False")
             offer = request.POST.get("offer","False")
             hotelid = request.session["hotelid"]
             if 'updateitem' in request.POST and "uitemid" in request.session:
                 itemid = request.session["uitemid"]
                 if img:
-                    db.child("hotel").child(hotelid).child("items").child(itemid).update({"itemimage": img})
+                    store.child("images").child(hotelid).child("items/" + itemid+".jpg").put(img)
+                    img_url = store.child("images").child(hotelid).child("items/" + itemid+".jpg").get_url(None)
+                    db.child("Hotel").child(hotelid).child("items").child(itemid).update({"itemimage": img_url})
                 update_db = {"itemname":itemname,"itemcategory":catagory,"itemdescription":desc,"itemprice":price,"offer":offer,"status":item_status}
                 db.child("Hotel").child(hotelid).child("items").child(itemid).update(update_db)
                 del request.session["uitemid"]
@@ -194,11 +218,13 @@ def additem(request):
                     titem = {}
                 titem = len(titem)+1
                 itemid = "item" + str(titem)
-                add_db = {"itemname":itemname,"itemcategory":catagory,"itemdescription":desc,"itemimage":img,"itemprice":price,"offer":offer,"status":item_status}
+                store.child("images").child(hotelid).child("items/" + itemid+".jpg").put(img)
+                img_url = store.child("images").child(hotelid).child("items/" + itemid+".jpg").get_url(None)
+                add_db = {"itemname":itemname,"itemcategory":catagory,"itemdescription":desc,"itemimage":img_url,"itemprice":price,"offer":offer,"status":item_status}
                 db.child("Hotel").child(hotelid).child("items").child(itemid).set(add_db)
                 err = {"status":"success","one":"Well done!","two":"You successfully added item."}
-        except:
-            err = {"status":"danger","one":"Try again!","two":"Something went wrong."}
+        #except:
+            #err = {"status":"danger","one":"Try again!","two":"Something went wrong."}
     try:
         del request.session["uitemid"]
     except KeyError:
